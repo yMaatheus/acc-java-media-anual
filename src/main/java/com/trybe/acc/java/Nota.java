c30dbd064945d1c0d1ceaf92eb245fe4083dfbae
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-07 17:49:31 -0300

    feat: create method calcularMedia at class Nota

--- src/main/java/com/trybe/acc/java/Nota.java
+++ src/main/java/com/trybe/acc/java/Nota.java
@@ -1,5 +1,12 @@
 package com.trybe.acc.java;
 
+/**
+ * Class Nota.
+ */
 public class Nota {
+
+  public double calcularMedia(double nota1, double nota2, double nota3, double nota4) {
+    return (nota1 + nota2 + nota3 + nota4) / 4;
+  }
 
 }
