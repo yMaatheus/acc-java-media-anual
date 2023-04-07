dfdbe6451a830d8bba36243ba6681d97159479ae
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-07 17:50:47 -0300

    feat: input scanner questions and show grade avarage

--- src/main/java/com/trybe/acc/java/Main.java
+++ src/main/java/com/trybe/acc/java/Main.java
@@ -1,10 +1,54 @@
 package com.trybe.acc.java;
 
+import java.util.Scanner;
+
+/**
+ * Class Main.
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  /**
+   * Initial Method.
+   */
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-	}
+    System.out.println("Qual é o seu nome?");
+
+    String name = scanner.next();
+
+    System.out.println("Entre com as notas das provas:");
+
+    try {
+      System.out.println("Avaliação 1:");
+
+      double nota1 = Double.parseDouble(scanner.next());
+
+      System.out.println("Avaliação 2:");
+
+      double nota2 = Double.parseDouble(scanner.next());
+
+      System.out.println("Avaliação 3:");
+
+      double nota3 = Double.parseDouble(scanner.next());
+
+      System.out.println("Avaliação 4:");
+
+      double nota4 = Double.parseDouble(scanner.next());
+
+      Nota nota = new Nota();
+      double media = nota.calcularMedia(nota1, nota2, nota3, nota4);
+
+      StringBuilder sb = new StringBuilder();
+
+      sb.append("Olá, ").append(name).append(", sua média é ").append(media);
+
+      System.out.println(sb.toString());
+    } catch (Exception e) {
+      System.out.println("Entre com uma opção válida!");
+    }
+
+    scanner.close();
+  }
 
 }
